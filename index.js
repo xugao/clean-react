42c9b4adcd05eb4438d81d6129b39ced33ff534e
Xu Gao <xugao0131@hotmail.com>
2016-06-10 09:42:24 -0700

    basic setup lint, react, es6

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      Hello React!
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('app'));
