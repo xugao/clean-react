e851b005fb7a2bc5622bc7de32194de97d4a3c27
Xu Gao <xugao0131@hotmail.com>
2016-06-10 10:07:41 -0700

    setup build and local dev (all settings are completed for coding)

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + "/app/index.html",
+  filename: 'index.html',
+  inject: 'body',
+});
+
+const config = {
+  entry: './app/index.js',
+  output: {
+    path: __dirname + "/dist",
+    filename: 'index_bundle.js',
+  },
+  module: {
+    loaders: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+        { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ],
+  },
+  plugins: [HtmlWebpackPluginConfig],
+};
+
+module.exports = config;
